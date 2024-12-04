47ac0253b549c590afe1837960f6f44f835ac813
michael <michael.g.george@gmail.com>
2024-12-04 23:16:26 +0530

    feat(controllers): group bookstore controllers

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,6 +2,14 @@ package controllers
 
 import "net/http"
 
+type BookstoreController struct {
+	CreateBook  http.HandlerFunc
+	GetBooks    http.HandlerFunc
+	GetBookById http.HandlerFunc
+	UpdateBook  http.HandlerFunc
+	DeleteBook  http.HandlerFunc
+}
+
 func CreateBook(w http.ResponseWriter, r *http.Request) {
 
 }
