7e734869e213b882253ae1730096da0f1f7c728e
michael <michael.g.george@gmail.com>
2024-12-04 12:33:33 +0530

    feat(middleware): Add SetJSONContentType

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,10 @@
 package utils
+
+import "net/http"
+
+func SetJSONContentType(n http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		n.ServeHTTP(w, r)
+	})
+}
