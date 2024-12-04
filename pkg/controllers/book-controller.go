43cce711e13da626449a23b68629e3bc97ef3603
michael <michael.g.george@gmail.com>
2024-12-04 11:36:18 +0530

    feat(pkg/controllers): Add stub for UpdateBook

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -13,3 +13,7 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 func GetBookById(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+}
