72251cc9ddbd3b7741b0d65002122af66a25311a
michael <michael.g.george@gmail.com>
2024-12-04 11:37:09 +0530

    feat(pkg/controllers): Add stub for DeleteBook

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -17,3 +17,7 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
