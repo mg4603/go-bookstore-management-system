18b32ea92dbb0e0e99db1d7fd64e107abf65a077
michael <michael.g.george@gmail.com>
2024-12-04 23:21:33 +0530

    Test(routes): test RegisterBookStoreRoutes

--- pkg/routes/bookstore-routes_test.go
+++ pkg/routes/bookstore-routes_test.go
@@ -1,6 +1,13 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/mg4603/go-bookstore-management-system/pkg/controllers"
+)
 
 // mock controllers
 func mockCreateBook(w http.ResponseWriter, r *http.Request) {
@@ -23,4 +30,78 @@ func mockGetBookById(w http.ResponseWriter, r *http.Request) {
 func mockGetBooks(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Books fetched"))
+}
+
+func TestRegisterBookstoreRoutes(t *testing.T) {
+	mockHandlers := &controllers.BookstoreController{
+		CreateBook:  mockCreateBook,
+		UpdateBook:  mockUpdateBook,
+		DeleteBook:  mockDeleteBook,
+		GetBooks:    mockGetBooks,
+		GetBookById: mockGetBookById,
+	}
+
+	r := mux.NewRouter()
+	RegisterBookstoreRoutes(r, mockHandlers)
+
+	tests := []struct {
+		name           string
+		method         string
+		url            string
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name:           "GET BOOKS route",
+			method:         "GET",
+			url:            "/books/",
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Books fetched",
+		},
+		{
+			name:           "GET BOOK BY ID route",
+			method:         "GET",
+			url:            "/books/1",
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Book fetched",
+		},
+		{
+			name:           "DELETE BOOK route",
+			method:         "DELETE",
+			url:            "/books/1",
+			expectedStatus: http.StatusNoContent,
+			expectedBody:   "",
+		},
+		{
+			name:           "UPDATE BOOK route",
+			method:         "PUT",
+			url:            "/books/1",
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Book Updated",
+		},
+		{
+			name:           "CREATE BOOK route",
+			method:         "POST",
+			url:            "/books/",
+			expectedStatus: http.StatusCreated,
+			expectedBody:   "Book Created",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(tt.method, tt.url, nil)
+			rec := httptest.NewRecorder()
+			r.ServeHTTP(rec, req)
+
+			if rec.Code != tt.expectedStatus {
+				t.Errorf("Expected Status = %v; got  %v", tt.expectedStatus, rec.Code)
+			}
+			if rec.Body.String() != tt.expectedBody {
+				t.Errorf("Expected body = %v; got %v", tt.expectedBody, rec.Body.String())
+			}
+			if contentTypeHeader := rec.Header().Get("Content-Type"); contentTypeHeader != "application/json" {
+				t.Errorf("Expected application/json content-type header; got %v", contentTypeHeader)
+			}
+		})
+	}
 }
