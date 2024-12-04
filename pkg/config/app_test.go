6dfc0368499aaf8012b20db6ba2648fb5dc4c47e
michael <michael.g.george@gmail.com>
2024-12-05 04:58:32 +0530

    test(mock): Add mockOpenWithError to simulate DB connection failure

--- pkg/config/app_test.go
+++ pkg/config/app_test.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"errors"
+
 	_ "gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,4 +12,8 @@ var mockDB *gorm.DB
 func mockOpen(dialector gorm.Dialector, config *gorm.Config) (*gorm.DB, error) {
 	mockDB := &gorm.DB{}
 	return mockDB, nil
+}
+
+func mockOpenWithError(dialector gorm.Dialector, config *gorm.Config) (*gorm.DB, error) {
+	return nil, errors.New("Mocked connection error")
 }
