7e602da27497045f29d02fc58c9760db35a214dd
michael <michael.g.george@gmail.com>
2024-12-06 05:18:53 +0530

    feat(models): Define Book model for database schema

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,12 @@
 package models
+
+import "gorm.io/gorm"
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:"" json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
