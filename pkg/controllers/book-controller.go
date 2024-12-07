92ccf11bfb6177647c1c41c408c68951785b4606
michael <michael.g.george@gmail.com>
2024-12-08 02:44:18 +0530

    refactor: convert handler funcs to methods on BookstoreController

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -3,29 +3,24 @@ package controllers
 import "net/http"
 
 type BookstoreController struct {
-	CreateBook  http.HandlerFunc
-	GetBooks    http.HandlerFunc
-	GetBookById http.HandlerFunc
-	UpdateBook  http.HandlerFunc
-	DeleteBook  http.HandlerFunc
 }
 
-func CreateBook(w http.ResponseWriter, r *http.Request) {
+func (c *BookstoreController) CreateBook(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func GetBooks(w http.ResponseWriter, r *http.Request) {
+func (c *BookstoreController) GetBooks(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func GetBookById(w http.ResponseWriter, r *http.Request) {
+func (c *BookstoreController) GetBookById(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func UpdateBook(w http.ResponseWriter, r *http.Request) {
+func (c *BookstoreController) UpdateBook(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func DeleteBook(w http.ResponseWriter, r *http.Request) {
+func (c *BookstoreController) DeleteBook(w http.ResponseWriter, r *http.Request) {
 
 }
