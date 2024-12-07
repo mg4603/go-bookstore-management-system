2498919c6c6733c68f4c430820d13e00402e80b6
michael <michael.g.george@gmail.com>
2024-12-07 05:42:40 +0530

    feat(models): add CreateBook for adding books to db

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"gorm.io/gorm"
 )
 
@@ -9,4 +11,14 @@ type Book struct {
 	Name        string `gorm:"not null" json:"name"`
 	Author      string `gorm:"not null" json:"author"`
 	Publication string `gorm:"not null" json:"publication"`
+}
+
+func CreateBook(b *Book, db *gorm.DB) error {
+	if b.Author == "" || b.Name == "" || b.Publication == "" {
+		return errors.New("missing required fields")
+	}
+	if result := db.Create(b); result.Error != nil {
+		return result.Error
+	}
+	return nil
 }
