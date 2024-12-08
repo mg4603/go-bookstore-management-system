15af1f5121ceb30bafb1fa2f21ef6daed22e8ff8
michael <michael.g.george@gmail.com>
2024-12-09 03:42:48 +0530

    refactor(controllers): remove setup fn for mock db

--- pkg/controllers/book-controller_test.go
+++ pkg/controllers/book-controller_test.go
@@ -1,19 +1,1 @@
 package controllers
-
-import (
-	"github.com/mg4603/go-bookstore-management-system/pkg/models"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-)
-
-func setup() (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
-	if err != nil {
-		return nil, err
-	}
-
-	if err := db.AutoMigrate(&models.Book{}); err != nil {
-		return nil, err
-	}
-	return db, nil
-}
