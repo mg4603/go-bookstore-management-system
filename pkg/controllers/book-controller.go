9e76fe25e900f86a62cb5ec97b598771489dd781
michael <michael.g.george@gmail.com>
2024-12-10 11:27:37 +0530

    feat(controllers): implement GetBookByIdHandler

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/mg4603/go-bookstore-management-system/pkg/models"
 	"github.com/mg4603/go-bookstore-management-system/pkg/utils"
 )
@@ -49,7 +52,35 @@ func GetBooksHandler(db *models.DBModel) http.HandlerFunc {
 
 func GetBookByIdHandler(db *models.DBModel) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		bookId, ok := vars["id"]
 
+		if !ok {
+			utils.HandleError(w, http.StatusBadRequest, "required field (id) is missing")
+			return
+		}
+
+		ID, err := strconv.ParseInt(bookId, 0, 0)
+		if err != nil {
+			utils.HandleError(w, http.StatusBadRequest, "bad input: couldn't parse int id, from string bookId: "+bookId)
+			return
+		}
+
+		bookDetails, err := db.GetBookById(ID)
+
+		if err != nil {
+			if err.Error() == fmt.Sprintf("book with ID %d not found", ID) {
+				utils.HandleError(w, http.StatusNotFound, fmt.Sprintf("book with id %d does not exist in database", ID))
+			} else {
+				utils.HandleError(w, http.StatusInternalServerError, fmt.Sprintf("error occured while trying to fetch record from db: %s", err.Error()))
+			}
+			return
+		}
+
+		if err := json.NewEncoder(w).Encode(bookDetails); err != nil {
+			utils.HandleError(w, http.StatusInternalServerError, fmt.Sprintf("error occurred while encoding response: %s", err.Error()))
+			return
+		}
 	}
 }
 
